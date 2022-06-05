cd9043e8a5f8d246d5f74aea61691d5496f5d8cf
turner-kl <angra5296@gmail.com>
2022-06-06 08:05:51 +0900

    4.3

---
+++ the-rust-programming-language/ch4/slice/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+let mut s = String::from("Hello, world!");
+
+let _word = first_word(&s);
+
+s.clear()
+}
+
+fn first_word(s: &String)-> usize{
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return i;
+        }
+    }
+
+    s.len()
+}
