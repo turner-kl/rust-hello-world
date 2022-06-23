deda80233c2529fb8b027dddfe8b3cd344dacfba
turner-kl <angra5296@gmail.com>
2022-06-23 09:26:15 +0900

    arg

--- rust-in-action/ch2/grep-lite/src/main.rs
+++ rust-in-action/ch2/grep-lite/src/main.rs
@@ -1,7 +1,21 @@
+use clap::{App, Arg};
+use regex::Regex;
+
 fn main() {
-    let ctx_lines = 2;
-    let needle = "oo";
-    let haystack = "\
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let quote = "\
     Every face, every shop, 
     bedroom window, public-house, and 
     dark square is a picture
@@ -10,39 +24,11 @@ fn main() {
     What do we seek
     through millions of pages?";
 
-    let mut tags: Vec<usize> = vec![];
-
-    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
-
-    for (i, line) in haystack.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(i);
-            let v = Vec::with_capacity(2 * ctx_lines + 1);
-            ctx.push(v);
-        }
-    }
-
-    if tags.is_empty() {
-        return;
-    }
-
-    for (i, line) in haystack.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(ctx_lines);
-            let upper_bound = tag + ctx_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(line);
-                let local_ctx = (i, line_as_string);
-                ctx[j].push(local_ctx);
-            }
-        }
-    }
-
-    for local_ctx in ctx.iter() {
-        for &(i, ref line) in local_ctx.iter() {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+    for line in quote.lines() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
