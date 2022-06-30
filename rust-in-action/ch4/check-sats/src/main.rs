c63b4855ffc81ee83f3d48f5faab864c592ba695
turner-kl <angra5296@gmail.com>
2022-07-01 07:28:48 +0900

    参照

--- rust-in-action/ch4/check-sats/src/main.rs
+++ rust-in-action/ch4/check-sats/src/main.rs
@@ -3,28 +3,54 @@
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
+    mailbox: Mailbox,
 }
 
 #[derive(Debug)]
 enum StatusMessage {
     OK,
 }
-
-fn check_status(sat_id: CubeSat) -> CubeSat {
-    println!("{:?}:{:?}", sat_id, StatusMessage::OK);
-    sat_id
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
 }
 
+type Message = String;
+
+struct GroungStation;
+
+impl GroungStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
+
+impl CubeSat {
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
+}
+
+// fn check_status(sat_id: CubeSat) -> CubeSat {
+//     println!("{:?}:{:?}", sat_id, StatusMessage::OK);
+//     sat_id
+// }
+
 fn main() {
-    let sat_a = CubeSat { id: 0 };
-    let sat_b = CubeSat { id: 1 };
-    let sat_c = CubeSat { id: 2 };
+    let base = GroungStation {};
+    let mut sat_a = CubeSat {
+        id: 0,
+        mailbox: Mailbox { messages: vec![] },
+    };
 
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
+    println!("t0: {:?}", sat_a);
 
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
+    base.send(&mut sat_a, Message::from("hello there!"));
+
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+
+    println!("msg: {:?}", msg);
 }
