a5c812e7aeb30c999b7cdf807a9625d7a47f7344
turner-kl <angra5296@gmail.com>
2022-07-03 10:29:20 +0900

    match

---
+++ the-rust-programming-language/ch6-match/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => 25,
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
