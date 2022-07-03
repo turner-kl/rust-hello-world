e43502d83a02ae4bcc2cff0270c888872d6acc9e
turner-kl <angra5296@gmail.com>
2022-07-03 09:59:01 +0900

    enum

---
+++ the-rust-programming-language/ch6-enum/src/main.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    println!("Hello, world! {:?}", home);
+}
