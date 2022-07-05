4cd259edd18c57311b4fe138cf3e81829966da39
turner-kl <angra5296@gmail.com>
2022-07-06 08:18:14 +0900

    ch6

---
+++ the-rust-programming-language/if-let/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let some_u8_value = Some(3u8);
+
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    println!("Hello, world!");
+}
